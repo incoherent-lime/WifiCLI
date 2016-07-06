124b8fc892262a51287e54bb6c6c2b6873c0a61a
lime <lime@limeTop.my.domain>
2016-07-07 00:49:29 +0100

    added initial derpy c file, might chanGe to ASM soon

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_CMN_LEN 100
+
+int main(int argc, char *argv[])
+{
+	    char cmd[MAX_CMN_LEN] = "", **p;
+
+system("ifconfig iwm0 scan");
+							    return 0;
+}
